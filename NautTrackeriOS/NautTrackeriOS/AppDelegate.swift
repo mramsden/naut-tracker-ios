c0bdad9b229c731a4e500eb72b0a18085295034d
Marcus Ramsden <marcus.ramsden@gmail.com>
2017-03-13 21:37:19 +0000

    Create ios app skeleton

---
+++ NautTrackeriOS/NautTrackeriOS/AppDelegate.swift
@@ -0,0 +1,22 @@
+//
+//  AppDelegate.swift
+//  NautTrackeriOS
+//
+//  Created by Marcus Ramsden on 13/03/2017.
+//  Copyright © 2017 Marcus Ramsden. All rights reserved.
+//
+
+import UIKit
+
+@UIApplicationMain
+class AppDelegate: UIResponder, UIApplicationDelegate {
+
+    var window: UIWindow?
+
+    func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplicationLaunchOptionsKey: Any]?) -> Bool {
+
+        return true
+    }
+
+}
+
